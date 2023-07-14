cdf0f28795f8b3875446c150982337e2df2d548b
Sainz <Sainz@DESKTOP-OMQD4GM>
2023-07-14 10:30:30 +0300

    add comm

--- dds_control/Dds.cpp
+++ dds_control/Dds.cpp
@@ -6,17 +6,20 @@ Dds::Dds()
 
 int Dds::init()
 {
-    return 0;
+    return 0;   //TODO: NOT IMPLEMENTATION.
 }
 
 void Dds::setFreq(int freq)
-{
+{ 
+    //TODO: NOT IMPLEMENTATION.
 }
 
 void Dds::setFi(int fi)
 {
+    //TODO: NOT IMPLEMENTATION.
 }
 
 void Dds::setA(int a)
 {
+    //TODO: NOT IMPLEMENTATION.
 }
