529d1cd87e9cc084edde35e4b7021c771cd729c9
Sainz <Sainz@DESKTOP-OMQD4GM>
2023-07-14 09:37:24 +0300

    fix encoding file

--- dds_control/Command.h
+++ dds_control/Command.h
@@ -10,7 +10,7 @@ public:
 	static const unsigned char WRI = 0b00100000;	//Запись addr в адресный регистр по адресу в адресном регистре с последующей инкрементацией адресного регистра 
 	static const unsigned char SETAFT = 0b00100000;	//Запись addr в адресный регистр с выборкой значения регистра по адресу addr в сдвиговый регистр
 
-	Command(const unsigned char cmd);
+	Command(const unsigned char cmd); 
 
 	unsigned char getByte(int i);
 };
