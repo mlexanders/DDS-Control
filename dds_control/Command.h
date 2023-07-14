16fdd1e987e124f4f8f87fc0b5666158aa7db24d
Sainz <Sainz@DESKTOP-OMQD4GM>
2023-07-14 09:35:20 +0300

    fix comments

--- dds_control/Command.h
+++ dds_control/Command.h
@@ -4,11 +4,11 @@ class Command {
 private:
 	unsigned char _cmd;
 public:
-	static const unsigned char NOP = 0b00000000;		//Нет операции. Значение data игнорируется.
-	static const unsigned char SETA = 0b00010000;	//Запись addr в адресный регистр. 
-	static const unsigned char WR = 0b00100000;		//Запись addr в адресный регистр. 
-	static const unsigned char WRI = 0b00100000;		//Запись data в регистр по адресу в адресном регистре с последующей инкрементацией адресного регистра
-	static const unsigned char SETAFT = 0b00100000;	//Запись addr в адресный регистр c выборкой значения регистра по адресу addr в сдвиговый регистр.
+	static const unsigned char NOP = 0b00000000;	//Нет операции, data игнорируется
+	static const unsigned char SETA = 0b00010000;	//Запись addr в адресный регистр 
+	static const unsigned char WR = 0b00100000;		//Запись data в регистр по адрессу в адресном простравнстве
+	static const unsigned char WRI = 0b00100000;	//Запись addr в адресный регистр по адресу в адресном регистре с последующей инкрементацией адресного регистра 
+	static const unsigned char SETAFT = 0b00100000;	//Запись addr в адресный регистр с выборкой значения регистра по адресу addr в сдвиговый регистр
 
 	Command(const unsigned char cmd);
 
