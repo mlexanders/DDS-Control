a06040090a79333705a8f2c0305848c6ab87ec95
Sainz <mlexanders@outlook.com>
2023-07-20 03:20:31 +0300

    cleanup

--- DdsBuilderExample/DdsBuilderExample.cpp
+++ DdsBuilderExample/DdsBuilderExample.cpp
@@ -13,11 +13,14 @@ Dds dds = DdsBuilder::useSerialController(&write, &chipSelect, &clock).getDds();
 
 int main()
 {
-    if (dds.init() == 1) return 1;
+    if (dds->init() == 1) return 1;
 
-	dds.setFreq(300);
-    dds.setA(1);
-    dds.setFi(5);
+    dds->setFreq(300);
+    dds->setFreq(2);
+    dds->setFreq(1);
+    dds->setA(1);
+    dds->setFi(5);
+    return 0;
 }
 
 
